355b2c84fd35c082036f69966d42f2bea6c64ef1
Riglanto <jaaroos@gmail.com>
2017-07-19 14:51:01 +0200

    Scss introduced

--- src/app/search-list/search-list.component.ts
+++ src/app/search-list/search-list.component.ts
@@ -10,7 +10,7 @@ import { SharedService } from './../shared.service';
 @Component({
   selector: 'app-search-list',
   templateUrl: './search-list.component.html',
-  styleUrls: ['./search-list.component.css']
+  styleUrls: ['./search-list.component.scss']
 })
 export class SearchListComponent implements OnInit {
 
